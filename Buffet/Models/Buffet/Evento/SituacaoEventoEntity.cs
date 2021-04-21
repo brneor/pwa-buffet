23e53b366131eab83c5df5c3005161d4103a9484
Breno Reis <breno.dk@gmail.com>
2021-04-21 13:44:22 -0300

    Criação da entidade Situação Evento.

---
+++ Buffet/Models/Buffet/Evento/SituacaoEventoEntity.cs
@@ -0,0 +1,8 @@
+namespace Buffet.Models.Buffet.Evento
+{
+    public class SituacaoEventoEntity
+    {
+        public string Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
