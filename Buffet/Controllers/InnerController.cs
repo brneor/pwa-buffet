1507b083341fbee7ff979354bfaeed181d2e8131
Breno Reis <breno.dk@gmail.com>
2021-05-17 23:08:11 -0300

    Alterações no CRUD de evento.

--- Buffet/Controllers/InnerController.cs
+++ Buffet/Controllers/InnerController.cs
@@ -1,16 +1,25 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Buffet.Data;
+using Buffet.Models.Acesso;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Buffet.Controllers
 {
     public class InnerController : Controller
     {
+        private readonly DatabaseContext _context;
+
         public IActionResult Start()
         {
             return View();
         }
 
-        public new IActionResult User()
+        public async Task<IActionResult> User()
         {
+            var login = HttpContext.User.Identity.Name;
+            // var user = _context.Users.Where(u => u.UserName.Equals(login));
             return View();
         }
 
